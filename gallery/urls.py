2096b53182b288e284b9d2a808dcfa323c3aa72c
nasirul <mdnasirulislamnahidweb@gmail.com>
2026-01-20 19:41:51 +0600

    Fixed template and URLs

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,8 +4,15 @@ from . import views
 app_name = "gallery"
 
 urlpatterns = [
-   path('', views.gallery_home, name='gallery_home'),
-    path('photos/', views.photo_gallery, name='photo_list'),
-    path('videos/', views.video_gallery, name='video_list'),
+    path('', views.gallery_home, name='gallery_home'),
+    # Photo CRUD
+    path('photo/<int:pk>/', views.photo_detail, name='photo_detail'),
+    path('photo/<int:pk>/update/', views.photo_update, name='photo_update'),
+    path('photo/<int:pk>/delete/', views.photo_delete, name='photo_delete'),
+
+    # Video CRUD
+    path('video/<int:pk>/', views.video_detail, name='video_detail'),
+    path('video/<int:pk>/update/', views.video_update, name='video_update'),
+    path('video/<int:pk>/delete/', views.video_delete, name='video_delete'),
 ]
     
--- gallery/views.py
+++ gallery/views.py
@@ -1,20 +1,71 @@
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView
-from django.shortcuts import render
 from .models import Photo, Video
+from .forms import PhotoForm, VideoForm
+from django.db.models import Q
 
+# ========== Gallery Home ========== 
 def gallery_home(request):
-    return render(request, 'gallery/base.html')
+    search_query = request.GET.get('search', '')
 
+    photos = Photo.objects.all()
+    videos = Video.objects.all()
 
+    if search_query:
+        photos = photos.filter(title__icontains=search_query)
+        videos = videos.filter(title__icontains=search_query)
 
-def photo_gallery(request):
-    photos = Photo.objects.all().order_by('-created_at')
-    return render(request, 'gallery/photo.html', {'photos': photos})
+    return render(request, 'gallery/gallery_home.html', {
+        'photos': photos,
+        'videos': videos,
+        'search_query': search_query
+    })
 
+# ========== Photo CRUD ==========
+def photo_detail(request, pk):
+    photo = get_object_or_404(Photo, pk=pk)
+    return render(request, 'gallery/photo_detail.html', {'photo': photo})
 
-def video_gallery(request):
-    videos = Video.objects.all().order_by('-created_at')
-    return render(request, 'gallery/video.html', {'videos': videos})
+def photo_update(request, pk):
+    photo = get_object_or_404(Photo, pk=pk)
+    if request.method == 'POST':
+        form = PhotoForm(request.POST, request.FILES, instance=photo)
+        if form.is_valid():
+            form.save()
+            return redirect('gallery:gallery_home')
+    else:
+        form = PhotoForm(instance=photo)
+    return render(request, 'gallery/photo_form.html', {'form': form, 'photo': photo})
+
+def photo_delete(request, pk):
+    photo = get_object_or_404(Photo, pk=pk)
+    if request.method == 'POST':
+        photo.delete()
+        return redirect('gallery:gallery_home')
+    return render(request, 'gallery/photo_confirm_delete.html', {'photo': photo})
+
+# ========== Video CRUD ==========
+def video_detail(request, pk):
+    video = get_object_or_404(Video, pk=pk)
+    return render(request, 'gallery/video_detail.html', {'video': video})
+
+def video_update(request, pk):
+    video = get_object_or_404(Video, pk=pk)
+    if request.method == 'POST':
+        form = VideoForm(request.POST, request.FILES, instance=video)
+        if form.is_valid():
+            form.save()
+            return redirect('gallery:gallery_home')
+    else:
+        form = VideoForm(instance=video)
+    return render(request, 'gallery/video_form.html', {'form': form, 'video': video})
+
+def video_delete(request, pk):
+    video = get_object_or_404(Video, pk=pk)
+    if request.method == 'POST':
+        video.delete()
+        return redirect('gallery:gallery_home')
+    return render(request, 'gallery/video_confirm_delete.html', {'video': video})
 
 class VideoListView(ListView):
     model = Video
